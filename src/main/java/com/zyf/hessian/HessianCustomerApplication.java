96b530f9b1542afdfc5708fff16b7c2e86f59b95
zhaoyafeng <942361291@qq.com>
2020-05-14 16:15:47 +0800

    提交

---
+++ src/main/java/com/zyf/hessian/HessianCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.zyf.hessian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HessianCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HessianCustomerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/zyf/hessian/service/SayHelloHessian.java
@@ -0,0 +1,19 @@
+package com.zyf.hessian.service;
+
+
+/**
+ * <发布的第一个hesian测试接口>
+ * <功能详细描述>
+ * @author zyf
+ * @version 2020年5月14日15:51:54
+ * @see [相关类/方法] (可选)
+ **/
+public interface SayHelloHessian {
+
+    /**
+     *  接口测试方法
+     * @return
+     */
+    public String sayHello(String msg);
+	
+}
